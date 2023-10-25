e703c8b9effbb31f4a9090e11bec9e19bbd37865
wakasa51 <takashi.yasuma@takashi.yasuma>
2023-10-25 11:07:00 +0900

    .gitconfigはcpする

--- init_for_mac.sh
+++ init_for_mac.sh
@@ -13,4 +13,4 @@ ln -s ~/dotfiles/.bash_profile ~/.bash_profile
 ln -s ~/dotfiles/.bashrc ~/.bashrc
 ln -s ~/dotfiles/.vimrc ~/.vimrc
 ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
-ln -s ~/dotfiles/.gitconfig ~/.gitconfig
+cp ~/dotfiles/.gitconfig ~/.gitconfig
