1dcf3ff9996c6aeb362a5bc3bb3a41423345dbb0
wakasa51 <takashi.yasuma@takashi.yasuma>
2024-05-29 19:20:34 +0900

    順序を変更する

--- init_for_mac.sh
+++ init_for_mac.sh
@@ -1,16 +1,19 @@
 #!/bin/bash -e
+cp ./.gitconfig ~/.gitconfig
+ln -s ./.bash_profile ~/.bash_profile
+ln -s ./.bashrc ~/.bashrc
 
 curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh
+ln -s ./.config/fish/config.fish ~/.config/fish/config.fish
+
 brew install vim ripgrep fzf tmux git fzf fish fd bat
 $(brew --prefix)/opt/fzf/install
+
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+ln -s ./.vimrc ~/.vimrc
+
 curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher
 fisher install PatrickF1/fzf.fish
 
 # シンボリックリンクを貼る
-ln -s ~/dotfiles/.config/fish/config.fish ~/.config/fish/config.fish
-ln -s ~/dotfiles/.bash_profile ~/.bash_profile
-ln -s ~/dotfiles/.bashrc ~/.bashrc
-ln -s ~/dotfiles/.vimrc ~/.vimrc
-ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
-cp ~/dotfiles/.gitconfig ~/.gitconfig
+ln -s ./.tmux.conf ~/.tmux.conf
